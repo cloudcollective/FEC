94e7fb37be66393c1283304440008ae893fe1b8d
Sean Park <seansangpark@gmail.com>
2021-07-21 17:31:14 -0700

    Babel, React, Webpack installed. Set up test and index.html

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './test.jsx';
+
+ReactDOM.render(<Test />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'client', 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'client', 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'development',
+}
