a8aae4cc65501ec14088fa083174d21ee95dd00e
Calvin Chau <cachau33@gmail.com>
2021-07-22 15:06:41 -0700

    Create our module folders. Add index.jsx to all the modules. In app.jsx, import the relevant index.jsx files

---
+++ client/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import productDetails from './productDetails';
+import qa from './qa';
+import relatedProducts from './relatedProducts';
+import rr from './rr';
+
+const app = () => (
+  <h1>Hello from React</h1>
+);
+
+export default app;
