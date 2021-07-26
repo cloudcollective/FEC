080c943b0f39b4266ce3dcf0441efaf77bed7e0d
Julie Barwick <julie@juliebarwick.com>
2021-07-26 12:04:06 -0700

    Check proptypes for incoming question and answers

--- client/src/qa/components/QuestionsAnswersList.jsx
+++ client/src/qa/components/QuestionsAnswersList.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const QuestionsAnswersList = ({ questions, answers }) => (
-  <div>
-    This is the inside container for the QuestionsAnswersList
-  </div>
-);
+const QuestionsAnswersList = ({ questions, answers }) => {
+  console.log(answers);
+
+  return (
+    <div>
+      Product ID {questions.product_id}
+      {/* {questions.results} */}
+
+    </div>
+  );
+};
+
+QuestionsAnswersList.propTypes = {
+  questions: PropTypes.shape({
+    product_id: PropTypes.string,
+    results: PropTypes.arrayOf(PropTypes.shape({
+      question_id: PropTypes.number,
+      question_body: PropTypes.string,
+      question_date: PropTypes.string,
+      asker_name: PropTypes.string,
+      question_helpfulness: PropTypes.number,
+      reported: PropTypes.bool,
+    })),
+  }).isRequired,
+  answers: PropTypes.shape({
+    question: PropTypes.string,
+    results: PropTypes.arrayOf(PropTypes.shape({
+      answer_id: PropTypes.number,
+      body: PropTypes.string,
+      date: PropTypes.string,
+      answerer_name: PropTypes.string,
+      helpfulness: PropTypes.number,
+      photos: PropTypes.arrayOf(PropTypes.string),
+    })),
+  }).isRequired,
+};
 
 export default QuestionsAnswersList;
