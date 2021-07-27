749052b9175cc718fec119c5a35efd4e540cab32
Calvin Chau <cachau33@gmail.com>
2021-07-27 10:54:45 -0700

    Update index, and create basic layout of my index

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 // import ProductDetails from './productDetails';
 import QuestionsAnswers from './qa';
-// import RelatedProducts from './relatedProducts';
+import RelatedProducts from './relatedProducts';
 // import ReviewsRatings from './rr';
 
 const app = ({ data }) => (
-  // <ProductDetails data={data} />
-  // <RelatedProducts data={data} />
-  <QuestionsAnswers
-    questions={data.questions}
-    answers={data.answers}
-  />
+  <div>
+    {/* <ProductDetails data={data} /> */}
+    <div>
+      <RelatedProducts products={data} />
+    </div>
+    <div>
+      <QuestionsAnswers
+        questions={data.questions}
+        answers={data.answers}
+      />
+    </div>
+  </div>
 );
 
 export default app;
