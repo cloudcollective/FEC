afe5c0fac9ec2b5c74f63150873e4d17a724768f
Julie Barwick <julie@juliebarwick.com>
2021-07-27 12:04:03 -0700

    Fix tests to pass - updates coverage folder

--- client/src/qa/components/Modal.test.js
+++ client/src/qa/components/Modal.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Modal from './Modal';
 
 test('It should render', () => {
   render(<Modal />);
-  expect(screen.getByRole('dialog')).toBeInDocument();
+  expect(screen.getByRole('dialog')).toBeInTheDocument();
 });
