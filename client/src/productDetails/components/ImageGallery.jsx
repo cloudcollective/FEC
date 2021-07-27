9f9422e8f60164feda2ce7b959ca6bbfde1813e7
Sean Park <seansangpark@gmail.com>
2021-07-27 10:53:06 -0700

    Progress made on ImageGallery using styled-component

--- client/src/productDetails/components/ImageGallery.jsx
+++ client/src/productDetails/components/ImageGallery.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ImageGallery = (props) => (
-  <>
-    <h1>This is Image Gallery</h1>
-    <div>
-      <img src={props.photos[1].url} alt="first product image" />
-    </div>
-  </>
-);
+const Image = styled.img`
+  max-width: 100%;
+`;
+
+const Thumbnails = styled.img`
+  height: 92px;
+  margin: 10px 0;
+  cursor: pointer;
+  display: block;
+`;
+
+const MainProduct = styled.div`
+
+`;
+
+// props.photos
+// .results[0].photos
+const ImageGallery = (props) => {
+  const photos = props.photos.results[0].photos;
+  return (
+    <>
+      <div>
+        {photos.map((photo, index) => <Thumbnails src={photo.thumbnail_url} key={index} alt="thumbnails" />)}
+      </div>
+      <div>
+        <img src={photos[0].url} alt="first product image" />
+      </div>
+    </>
+  );
+};
 
 export default ImageGallery;
--- client/src/productDetails/index.js
+++ client/src/productDetails/index.js
@@ -1,31 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
 import ImageGallery from './components/ImageGallery';
 import ProductInformation from './components/ProductInformation';
 import StyleSelector from './components/StyleSelector';
 import AddToCart from './components/AddToCart';
-import ProductOverview from './components/ProductOverview';
+// import ProductOverview from './components/ProductOverview';
 
+const Body = styled.div`
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+`;
+
+const Content = styled.div`
+  float: left;
+  padding: 15px;
+`;
+
+const LeftColumn = styled.div`
+  float: left;
+  width: 75%;
+  border: 10px;
+  height: 300px;
+`;
+
+const RightColumn = styled.div`
+  float: left;
+  width: 25%;
+  border: 10px;
+`;
+
+// const Footer = styled.div`
+//   padding: 20px;
+//   text-align: center;
+//   background: #ddd;
+//   margin-top: 20px;
+// `;
 
 const ProductDetails = (props) => (
   //
-  <div>
-    <div>
-      <ImageGallery photos={props.productStyles.results[0].photos} />
-    </div>
+  <Body>
+    <Content>
+      <LeftColumn>
+        <ImageGallery photos={props.productStyles} />
+      </LeftColumn>
 
-    <div>
-      <ProductInformation product={props.product} styles={props.productStyles} />
-      <StyleSelector styles={props.productStyles} />
-      <AddToCart />
-    </div>
+      <RightColumn>
+        <ProductInformation product={props.product} styles={props.productStyles} />
+        <StyleSelector styles={props.productStyles} />
+        <AddToCart />
+      </RightColumn>
+    </Content>
 
-    <div>
+    {/* <Footer>
       <ProductOverview product={props.product} />
-    </div>
+    </Footer> */}
 
     {console.log(props.product)}
     {console.log(props.productStyles)}
-  </div>
+  </Body>
 );
 
 export default ProductDetails;
