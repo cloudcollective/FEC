c059a4fa9399c8a8019c83a9e1940617dd227b0e
Julie Barwick <julie@juliebarwick.com>
2021-07-27 11:44:34 -0700

    Create basic but incomplete form to be tested

---
+++ client/src/qa/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => (
+  <form>
+    <label htmlFor="question">
+      Your Question*
+      <textarea id="question" type="textarea" />
+    </label>
+  </form>
+);
+
+export default Form;
