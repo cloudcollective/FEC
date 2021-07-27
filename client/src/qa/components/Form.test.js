5917de3040bf9b39b0d57856b66ba2a00213d09b
Julie Barwick <julie@juliebarwick.com>
2021-07-27 11:45:22 -0700

    Implement one test to check if form is being rendered

---
+++ client/src/qa/components/Form.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Form from './Form';
+
+test('It should render a form to the page', () => {
+  render(<Form />);
+  expect(screen.getByRole('form')).toBeInDocument();
+});
