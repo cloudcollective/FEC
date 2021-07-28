55af7c36313f46f311ba82b8500adc9b4b20df21
Julie Barwick <julie@juliebarwick.com>
2021-07-27 20:09:41 -0700

    Develop form to be a controlled component

--- client/src/qa/components/Form.jsx
+++ client/src/qa/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const formGroup = {
   marginBottom: '1rem',
@@ -17,48 +17,80 @@ const formControl = {
   borderRadius: '.25rem',
 };
 
-const Form = () => (
-  <form aria-label="form">
-    <div className="formGroup" style={formGroup}>
-      <label htmlFor="question">
-        Your Question*
-        <textarea
-          style={formControl}
-          id="question"
-          rows="8"
-          cols="40"
-          maxLength="1000"
-        />
-      </label>
-    </div>
-    <div className="formGroup" style={formGroup}>
-      <label htmlFor="nickname">
-        What is your nickname*
-        <input
-          style={formControl}
-          type="text"
-          maxLength="60"
-          placeholder="Example: jackson11!"
-        />
-        <p><small>For privacy reasons, do not use your full name or email address</small></p>
-      </label>
-    </div>
-    <div className="formGroup" style={formGroup}>
-      <label htmlFor="email">
-        Your email*
-        <input
-          style={formControl}
-          type="email"
-          maxLength="60"
-          placeholder="Example: jack@email.com"
-        />
-        <p><small>For authentication reasons, you will not be emailed</small></p>
-      </label>
-    </div>
-    <button type="submit">Submit question</button>
-  </form>
-);
+const Form = () => {
+  const [questionBody, setQuestionBody] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
 
-// form textarea value should be in state
+  const clearState = () => {
+    setQuestionBody('');
+    setNickname('');
+    setEmail('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(questionBody, nickname, email);
+    clearState();
+  };
+
+  return (
+    <form aria-label="form">
+
+      <div className="formGroup" style={formGroup}>
+        <label htmlFor="question">
+          Your Question*
+          <textarea
+            style={formControl}
+            id="question"
+            rows="8"
+            cols="40"
+            maxLength="1000"
+            value={questionBody}
+            onChange={(e) => setQuestionBody(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <div className="formGroup" style={formGroup}>
+        <label htmlFor="nickname">
+          What is your nickname*
+          <input
+            style={formControl}
+            type="text"
+            maxLength="60"
+            placeholder="Example: jackson11!"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          <p><small>For privacy reasons, do not use your full name or email address</small></p>
+        </label>
+      </div>
+
+      <div className="formGroup" style={formGroup}>
+        <label htmlFor="email">
+          Your email*
+          <input
+            style={formControl}
+            type="email"
+            maxLength="60"
+            placeholder="Example: jack@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p><small>For authentication reasons, you will not be emailed</small></p>
+        </label>
+      </div>
+
+      <button
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Submit question
+      </button>
+
+    </form>
+  );
+};
 
 export default Form;
