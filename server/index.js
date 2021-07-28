fb40bf9e424a00b9975b6be92e8b55e0ed4c4a68
Calvin Chau <cachau33@gmail.com>
2021-07-28 10:28:01 -0700

    Create atlier.js to to process api request for products, reviews. QA, Cart, and other request can be added in the future. Update index so that it can porcess said request

--- server/index.js
+++ server/index.js
@@ -1,11 +1,85 @@
 const express = require('express');
+const path = require('path');
+const atlier = require('./atlier');
 
 const app = express();
-const path = require('path');
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
-const port = 3000;
 
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+
+  Promise.all([
+    atlier.getProductById(id),
+    atlier.getProductStylesById(id),
+    atlier.getRelatedProductsById(id),
+    atlier.getReviewsById(id),
+    atlier.getMetaReviewsById(id),
+  ])
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((error) => {
+      res.status(501).send(error);
+    });
+});
+
+app.get('/related/:id', (req, res) => {
+  const { id } = req.params;
+
+  Promise.all([
+    atlier.getProductById(id),
+    atlier.getProductStylesById(id),
+    atlier.getMetaReviewsById(id),
+  ])
+    .then((data) => {
+      const relatedData = {
+        id: data[0].id,
+        name: data[0].name,
+        category: data[0].category,
+        price: data[0].default_price,
+        features: data[0].features,
+        url: data[1].results[0].photos[0].url,
+        ratings: data[2].ratings,
+      };
+
+      res.status(201).send(relatedData);
+    })
+    .catch((error) => {
+      res.status(501).send(error);
+    });
+});
+
+app.get('/reviews/:id', (req, res) => {
+  const { id, sort } = req.query;
+
+  Promise.all([
+    atlier.getReviewsById(id, sort),
+    atlier.getMetaReviewsById(id),
+  ])
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((error) => {
+      res.status(501).send(error);
+    });
+});
+
+app.get('/reviews/meta', (req, res) => {
+  const { id } = req.params;
+
+  atlier.getMetaReviewsById(id)
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((error) => {
+      res.status(501).send(error);
+    });
+});
+
+const port = 3000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listening on port: ${port}`);
 });
