eb96b26303518e3fd9f110098d1360c0a5b73015
Julie Barwick <julie@juliebarwick.com>
2021-07-28 21:23:26 -0700

    Implement working, partially reusable form for accepting questions, handles form validation

--- client/src/qa/components/Form.jsx
+++ client/src/qa/components/Form.jsx
@@ -1,96 +1,122 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import Input from './common/Input';
+import TextareaInput from './common/TextareaInput';
+import Button from './common/Button';
 
-const formGroup = {
-  marginBottom: '1rem',
-};
-
-// taken from bootstrap
-const formControl = {
-  display: 'block',
-  width: '100%',
-  padding: '.375rem .75rem',
-  fontSize: '1rem',
-  lineHeight: '1.5',
-  color: '#495057',
-  backgroundColor: '#fff',
-  border: '1px solid #ced4da',
-  borderRadius: '.25rem',
-};
+const StyledFormGroup = styled.div`
+  margin-bottom: 1rem;
+`;
 
 const Form = () => {
-  const [questionBody, setQuestionBody] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [email, setEmail] = useState('');
+  const defaultQuestionInfo = {
+    question: '',
+    nickname: '',
+    email: '',
+  };
+  const [input, setInput] = useState(defaultQuestionInfo);
+  const { question, nickname, email } = input;
+
+  const [errors, setErrors] = useState(null);
 
   const clearState = () => {
-    setQuestionBody('');
-    setNickname('');
-    setEmail('');
+    setInput((prevState) => ({
+      ...prevState,
+      ...defaultQuestionInfo,
+    }));
+  };
+
+  const capitalizeFirst = (string) => (string[0].toUpperCase() + string.slice(1));
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const validate = () => {
+    const errorsFound = {};
+
+    if (question.trim() === '') {
+      errorsFound.question = 'Question is required';
+    }
+    if (nickname.trim() === '') {
+      errorsFound.nickname = 'Nickname is required';
+    }
+    if (email.trim() === '') {
+      errorsFound.email = 'Email is required';
+    }
+    return Object.keys(errorsFound).length === 0 ? null : errorsFound;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(questionBody, nickname, email);
+    const errorsFound = validate();
+    setErrors(errorsFound);
+    if (errorsFound) {
+      return;
+    }
+    // do some api call
     clearState();
   };
 
   return (
     <form aria-label="form">
+      <StyledFormGroup>
+        <TextareaInput
+          label="Your Question*"
+          name="question"
+          maxLength="1000"
+          value={question}
+          onChange={handleInput}
+        />
+      </StyledFormGroup>
 
-      <div className="formGroup" style={formGroup}>
-        <label htmlFor="question">
-          Your Question*
-          <textarea
-            style={formControl}
-            id="question"
-            rows="8"
-            cols="40"
-            maxLength="1000"
-            value={questionBody}
-            onChange={(e) => setQuestionBody(e.target.value)}
-          />
-        </label>
-      </div>
+      <StyledFormGroup>
+        <Input
+          name="nickname"
+          label="What is your nickname*"
+          placeholder="Example: jackson11!"
+          maxLength="60"
+          value={nickname}
+          onChange={handleInput}
+          afterInput="For privacy reasons, do not use your full name or email address"
+        />
+      </StyledFormGroup>
 
-      <div className="formGroup" style={formGroup}>
-        <label htmlFor="nickname">
-          What is your nickname*
-          <input
-            style={formControl}
-            type="text"
-            maxLength="60"
-            placeholder="Example: jackson11!"
-            value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
-          />
-          <p><small>For privacy reasons, do not use your full name or email address</small></p>
-        </label>
-      </div>
-
-      <div className="formGroup" style={formGroup}>
-        <label htmlFor="email">
-          Your email*
-          <input
-            style={formControl}
-            type="email"
-            maxLength="60"
-            placeholder="Example: jack@email.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <p><small>For authentication reasons, you will not be emailed</small></p>
-        </label>
-      </div>
-
-      <button
+      <StyledFormGroup>
+        <Input
+          name="email"
+          label="Your email*"
+          placeholder="Example: jack@email.com"
+          maxLength="60"
+          value={email}
+          onChange={handleInput}
+          afterInput="For authentication reasons, you will not be emailed"
+        />
+      </StyledFormGroup>
+      {errors
+        && (
+          <WarningText>
+            <p>You must enter the following:</p>
+            <ul>
+              {Object.keys(errors).map((error) => <li key={error}>{`${capitalizeFirst(error)}: ${errors[error]}`}</li>)}
+            </ul>
+          </WarningText>
+        )}
+      <Button
         type="submit"
+        label="Submit question"
         onClick={handleSubmit}
-      >
-        Submit question
-      </button>
-
+      />
     </form>
   );
 };
 
 export default Form;
+
+const WarningText = styled.div`
+  color: red;
+`;
