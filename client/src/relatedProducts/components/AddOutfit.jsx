b23e07c1447791b08728a2dbaf4e0a3733953679
Calvin Chau <cachau33@gmail.com>
2021-07-29 12:03:19 -0700

    Set up basic template, for add outfit. Will have to work on functionality

--- client/src/relatedProducts/components/AddOutfit.jsx
+++ client/src/relatedProducts/components/AddOutfit.jsx
@@ -4,9 +4,9 @@ import CardMaker from './CardMaker.jsx';
 const AddOutfit = ({ product }) => (
   <div className="custom-outfit-container">
     <div className="add-outfit-card">
-      <button type="button" onClick={CardMaker(product)}>Add Outfit</button>
+      <button type="button" onClick={() => (CardMaker({ product }))}>Add Outfit</button>
     </div>
   </div>
 );
-
+// Question after they click on add outfit, will it create a card component inside of AddOutfit?
 export default AddOutfit;
