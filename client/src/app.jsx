7cdfe9dbb39f58cc13512f055474755cd5d3e0a3
Calvin Chau <cachau33@gmail.com>
2021-07-28 17:58:53 -0700

    Refactored app,jsx into a class component. Will retrieve data once componeneDidMount.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,22 +1,82 @@
 import React from 'react';
+import axios from 'axios';
 // import ProductDetails from './productDetails';
-import QuestionsAnswers from './qa';
 import RelatedProducts from './relatedProducts';
+// import QuestionsAnswers from './qa';
 // import ReviewsRatings from './rr';
 
-const app = ({ data }) => (
-  <div>
-    {/* <ProductDetails data={data} /> */}
-    <div>
-      <RelatedProducts products={data} />
-    </div>
-    <div>
-      <QuestionsAnswers
-        questions={data.questions}
-        answers={data.answers}
-      />
-    </div>
-  </div>
-);
+class App extends React.components {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedProduct: {},
+      relatedProductIDs: [],
+      reviews: 'placeholder',
+    };
 
-export default app;
+    this.getProductData = this.getProductData.bind(this);
+    this.getRelatedProductData = this.getProductData.bind(this);
+  }
+
+  componentDidMount() {
+    // 25167
+    this.getProductData('25167');
+  }
+
+  getProductData(id) {
+    axios.get(`/products/${id}`)
+      .then((data) => {
+        this.setState({
+          selectedProduct: data.data[0],
+          relatedProductIds: data.data[2],
+          // relatedProductIds: [...new Set(data.data[2])],
+          reviews: data.data[3],
+        });
+      })
+      // .then(() => {
+      //   //
+      // })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      });
+  }
+
+  render() {
+    const { selectedProduct, relatedProductIds } = this.state;
+    return (
+      <div>
+        {/* <ProductDetails data={data} /> */}
+        <div>
+          <RelatedProducts
+            product={selectedProduct}
+            products={relatedProductIds}
+          />
+        </div>
+        {/* <div>
+         <QuestionsAnswers
+           questions={data.questions}
+           answers={data.answers}
+         />
+       </div> */}
+      </div>
+    );
+  }
+}
+
+// const app = ({ data }) => (
+//   <div>
+//     {/* <ProductDetails data={data} /> */}
+//     <div>
+//       <RelatedProducts products={data} />
+//     </div>
+//     {/* <div>
+//       <QuestionsAnswers
+//         questions={data.questions}
+//         answers={data.answers}
+//       />
+//     </div> */}
+//   </div>
+// );
+// export default app;
+export default App;
