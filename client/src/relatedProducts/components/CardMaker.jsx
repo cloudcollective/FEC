e2bf1e7bddabd51f55ce47e23092648b2ab14fbe
Calvin Chau <cachau33@gmail.com>
2021-07-29 12:04:17 -0700

    Set up basic design, will be working on css next.

--- client/src/relatedProducts/components/CardMaker.jsx
+++ client/src/relatedProducts/components/CardMaker.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const CardMaker = ({ products }) => {
-  console.log(products, 'This here are you temp. Products');
+const CardMaker = ({ products, product }) => {
+  // eslint-disable-next-line no-console
+  console.log(product, 'This here are you temp. Products');
   return (
-    <div>
-      nothing here yet
+    <div className="card">
+      <img src={`${product.thumbnail}`} alt="Product Img" />
+      <div className="category">
+        {`Category: ${product.category}`}
+      </div>
+      <div className="product-name">
+        {`Product Name: ${product.name}`}
+      </div>
+      <div className="price">
+        {`Price: $${product.default_price}`}
+      </div>
+      <div className="stars">
+        ★★★★★
+      </div>
     </div>
   );
 };
