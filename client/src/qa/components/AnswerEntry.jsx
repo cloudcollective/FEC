3ad8fe171db72f01fb1890ddd87312f4f0a578cb
Julie Barwick <julie@juliebarwick.com>
2021-07-29 20:30:47 -0700

    Implement working date-formatting function. WIP: username display

--- client/src/qa/components/AnswerEntry.jsx
+++ client/src/qa/components/AnswerEntry.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const AnswerEntry = ({ answer }) => {
-  console.log(answer);
+  const formatDate = (date) => {
+    const d = new Date(date);
+    const [day, year] = [d.getDate(), d.getFullYear()];
+    const month = d.toLocaleString('default', { month: 'long' });
+    return `${month} ${day}, ${year}`;
+  };
+
   return (
-    <p>{answer.body}</p>
-  )
-}
+    <>
+      <p>{answer.body}</p>
+      <p>by {answer.answerer_name}, {formatDate(answer.date)} | Helpful? <a href="#" onClick={()=>{}}>Yes</a> {'('}{answer.helpfulness}{')'} | <a href="#" onClick={()=>{}}>Report</a></p>
+    </>
+  );
+};
 //   <div style={divStyle}>
 //     <span>{answer}</span>
 //   </div>
@@ -17,5 +27,16 @@ const StyledSpan = styled.div`
   display: inline-block;
   border: 1px solid red;
 `;
+
+AnswerEntry.propTypes = {
+  answer: PropTypes.shape({
+    answer_id: PropTypes.number,
+    body: PropTypes.string,
+    date: PropTypes.string,
+    answerer_name: PropTypes.string,
+    helpfulness: PropTypes.number,
+    photos: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
 
 export default AnswerEntry;
