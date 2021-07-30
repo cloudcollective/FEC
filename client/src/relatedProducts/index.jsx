86748a5d3bcb6ecb1408c23b675698ec0172c426
Calvin Chau <cachau33@gmail.com>
2021-07-30 15:11:53 -0700

    Refactored my index.jsx so that all promises are fulfilled and returned in one sitting.

--- client/src/relatedProducts/index.jsx
+++ client/src/relatedProducts/index.jsx
@@ -1,48 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 // import PropTypes from 'prop-types';
 // 'prop-types' should be listed in the project's dependencies. Run 'npm i -S prop-types' to add it
 import CardMaker from './components/CardMaker';
 import AddOutfit from './components/AddOutfit';
 // refactor using hooks later
-const RelatedProductsContainer = ({ product, products }) => (
-  <div className="rp-container">
-    <div className="list-row-1">
-      <h4>Related Products</h4>
-      <div className="card-list">
-        <CardMaker
-          products={products}
-          product={product}
-        />
+
+const RPContainer = styled.div`
+border: solid 1px black;
+`;
+const CardList = styled.div`
+border: solid 1px black;
+padding: 10px;
+margin: 0 0 0 5px;
+`;
+const RelatedProductsContainer = ({ product, products }) => {
+  const arrayOfObj = products;
+  // console.log(product, products, 'test')
+  const renderElement = () => {
+    if (products !== undefined) {
+      return (<CardMaker product={product} />);
+    }
+  };
+  return (
+    <RPContainer>
+      <div className="list-row-1">
+        <h4><em>Related Products</em></h4>
+        <CardList>
+          {renderElement()}
+        </CardList>
       </div>
-    </div>
-    <div className="list-row-2">
-      <h4>Custom Outfits</h4>
-      <div className="card-list">
-        <AddOutfit product={product} />
+      <div className="list-row-2">
+        <h4><em>Custom Outfits</em></h4>
+        <CardList>
+          <AddOutfit product={product} />
+        </CardList>
       </div>
-    </div>
-  </div>
-);
+    </RPContainer>
+  );
+};
 
 // RelatedProductsContainer.propTypes = {
 //   products: PropTypes.object.isRequired,
 // };
 
 export default RelatedProductsContainer;
-/* List of Changes:
-1) All this work is being done in a separate branch
-2) Created a components folder
-3) Built out the skeleton of index.js
-4) Changed class to classname, as per Reacts recommendation
-5) Changed index.js to index.jsx due to:
-JSX not allowed in files with extension '.js'
-6) Uncommented out my Related Products Components in app.jsx
-7) Wrapped app.jsx in a div tag. Commented out ProductDetails, and fixed all auto-correctable errors
-8) Created CardMaker component.
-8.5) What the heck are prop.types? Learned Destructuring.
+/*
+          <CardMaker product={arrayOfObj[0]} />
+          <CardMaker product={arrayOfObj[1]} />
+          <CardMaker product={arrayOfObj[2]} />
+          <CardMaker product={arrayOfObj[3]} />
 */
-// const productId = products.data[0].id;
-// const relatedProductId = products.data[2];
-// const arrayOfProductsData = [];
-// const currentProductData = {};
-// getRelatedProductsData(productId, 'one')
