a57c3c0948f21c1a38736a94fd14e72804a575dc
Julie Barwick <julie@juliebarwick.com>
2021-07-31 11:38:14 -0700

    Remove hooks useState - to be decided on later

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-// import ProductDetails from './productDetails';
+import ProductDetails from './productDetails';
 import QuestionsAnswers from './qa';
 // import RelatedProducts from './relatedProducts';
 // import ReviewsRatings from './rr';
 
 const app = ({ data }) => {
-  // Default productId is 25172
-  const [productId, setProductId] = useState('25172');
+  // This will be the hooks version
+  // const [productId, setProductId] = useState('25172');
 
   return (
     <>
-      {/* <ProductDetails product={data.product} productStyles={data.productStyles} /> */}
+      <ProductDetails product={data.product} productStyles={data.productStyles} />
       {/* <RelatedProducts data={data} /> */}
       <QuestionsAnswers
-        productId={productId}
+        productId={'25172'}
         questions={data.questions}
         answers={data.answers}
       />
