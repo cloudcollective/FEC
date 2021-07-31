2dad61cd3df6b0b0a7ffe3b4b9028f1af71b1bdd
Julie Barwick <julie@juliebarwick.com>
2021-07-30 18:13:26 -0700

    Use other components to build the question entry - move modal window and button toggle here

--- client/src/qa/components/QuestionEntry.jsx
+++ client/src/qa/components/QuestionEntry.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Helpful from './Helpful';
+import AddAnswerBtn from './AddAnswerBtn';
+import useToggle from './common/useToggle';
+import AnswerModal from './AnswerModal';
 
-const QuestionEntry = ({ question }) => (
-  <>
-    <FirstCol><h3>Q: </h3></FirstCol>
-    <SecondCol><h3>{question.question_body}</h3></SecondCol>
-  </>
-);
+const QuestionEntry = ({ question, helpfulness, questionId }) => {
+  const answerToggle = useToggle();
+
+  return (
+    <>
+      <FirstCol><h3>Q: </h3></FirstCol>
+      <SecondCol><h3>{question}</h3></SecondCol>
+      <ThirdCol>
+        <Helpful
+          helpfulness={helpfulness}
+          id={questionId}
+        />
+        {' | '}
+        <AddAnswerBtn setIsVisible={answerToggle.toggle} />
+        <AnswerModal
+          isVisible={answerToggle.on}
+          setIsVisible={answerToggle.toggle}
+        />
+      </ThirdCol>
+    </>
+  );
+};
 
 const FirstCol = styled.div`
   grid-column: 1 / span 1
@@ -15,5 +36,15 @@ const FirstCol = styled.div`
 const SecondCol = styled.div`
   grid-column: 2 / span 1;
 `;
+
+const ThirdCol = styled.div`
+  grid-column: 3 / span 1;
+`;
+
+QuestionEntry.propTypes = {
+  question: PropTypes.string.isRequired,
+  helpfulness: PropTypes.number.isRequired,
+  questionId: PropTypes.number.isRequired,
+};
 
 export default QuestionEntry;
