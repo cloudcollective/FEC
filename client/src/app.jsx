a7825ab5854f90685ba0b6262c094d9a15b511c8
Julie Barwick <julie@juliebarwick.com>
2021-07-30 19:34:46 -0700

    Implement useState for a default productId number, pass to QA component

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import ProductDetails from './productDetails';
+import React, { useState } from 'react';
+// import ProductDetails from './productDetails';
 import QuestionsAnswers from './qa';
 // import RelatedProducts from './relatedProducts';
 // import ReviewsRatings from './rr';
 
-const app = ({ data }) => (
-  <>
-    <ProductDetails product={data.product} productStyles={data.productStyles} />
-    {/* <RelatedProducts data={data} /> */}
-    <QuestionsAnswers
-      questions={data.questions}
-      answers={data.answers}
-    />
-  </>
-);
+const app = ({ data }) => {
+  // Default productId is 25172
+  const [productId, setProductId] = useState('25172');
+
+  return (
+    <>
+      {/* <ProductDetails product={data.product} productStyles={data.productStyles} /> */}
+      {/* <RelatedProducts data={data} /> */}
+      <QuestionsAnswers
+        productId={productId}
+        questions={data.questions}
+        answers={data.answers}
+      />
+    </>
+  );
+};
 
 export default app;
