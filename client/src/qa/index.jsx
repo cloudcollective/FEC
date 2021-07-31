99023c4882038a75d312d6adf04f9f8c641fabec
Julie Barwick <julie@juliebarwick.com>
2021-07-30 18:06:30 -0700

    Move Answer Modal to a different component

--- client/src/qa/index.jsx
+++ client/src/qa/index.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import QuestionsAnswersList from './components/QuestionsAnswersList';
 import SearchQuestions from './components/SearchQuestions';
 import AddQuestionBtn from './components/AddQuestionBtn';
 import QuestionModal from './components/QuestionModal';
 import useToggle from './components/common/useToggle';
-import AddAnswerBtn from './components/AddAnswerBtn';
-import AnswerModal from './components/AnswerModal';
-
-const divStyle = {
-  border: '1px solid blue',
-};
 
 const QuestionsAnswersContainer = ({ questions, answers }) => {
-  const answerToggle = useToggle();
-  const questionToggle = useToggle();
+  const { on, toggle } = useToggle(false);
 
   return (
-    <div style={divStyle}>
+    <div>
       <h3>
         Questions &#38; Answers
       </h3>
@@ -26,15 +19,10 @@ const QuestionsAnswersContainer = ({ questions, answers }) => {
         questions={questions}
         answers={answers}
       />
-      <AddAnswerBtn setIsVisible={answerToggle.toggle} />
-      <AnswerModal
-        isVisible={answerToggle.on}
-        setIsVisible={answerToggle.toggle}
-      />
-      <AddQuestionBtn setIsVisible={questionToggle.toggle} />
+      <AddQuestionBtn setIsVisible={toggle} />
       <QuestionModal
-        isVisible={questionToggle.on}
-        setIsVisible={questionToggle.toggle}
+        isVisible={on}
+        setIsVisible={toggle}
       />
     </div>
   );
