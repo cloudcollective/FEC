f366b05b32fd4b8cfb0274c262d053e59cfb0c16
Julie Barwick <julie@juliebarwick.com>
2021-07-30 20:07:45 -0700

    Dynamically render the product name for the question modal

--- client/src/qa/components/QuestionModal.jsx
+++ client/src/qa/components/QuestionModal.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Form from './Form';
 import Modal from './common/Modal';
 
-const QuestionModal = ({ isVisible, setIsVisible }) => (
+const QuestionModal = ({ productName, isVisible, setIsVisible }) => (
   <Modal
     isVisible={isVisible}
     setIsVisible={setIsVisible}
   >
     <h3>Ask Your Question</h3>
+    <h4>
+      About the {productName}
+    </h4>
     <Form />
   </Modal>
 );
