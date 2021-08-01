5f8e3bb073760b986c6b7047af6342ac5b2bf9b3
Julie Barwick <julie@juliebarwick.com>
2021-07-31 17:56:33 -0700

    Fix proptypes to match new data input and add conditional for when still loading

--- client/src/qa/components/QuestionsAnswersList.jsx
+++ client/src/qa/components/QuestionsAnswersList.jsx
@@ -2,42 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import QuestionAndAnswerEntry from './QuestionAndAnswerEntry';
 
-const QuestionsAnswersList = ({ productName, questions, answers }) => (
+const QuestionsAnswersList = ({ productName, questions }) => (
   <div data-testid="qa-list">
-    {questions.results.map((question) => (
-      <QuestionAndAnswerEntry
-        productName={productName}
-        key={question.question_id}
-        question={question}
-        answers={answers.results}
-      />
-    ))}
+    {!questions.length
+      ? <h4>Loading questions...</h4>
+      : questions.map((question) => (
+        <QuestionAndAnswerEntry
+          productName={productName}
+          key={question.question_id}
+          question={question}
+          answers={Object.values(question.answers)}
+        />
+      ))}
   </div>
 );
 
 QuestionsAnswersList.propTypes = {
-  questions: PropTypes.shape({
-    product_id: PropTypes.string,
-    results: PropTypes.arrayOf(PropTypes.shape({
+  questions: PropTypes.oneOfType([PropTypes.object, PropTypes.arrayOf(
+    PropTypes.shape({
+      answers: PropTypes.shape({
+        answer_id: PropTypes.number,
+        body: PropTypes.string,
+        date: PropTypes.string,
+        answerer_name: PropTypes.string,
+        helpfulness: PropTypes.number,
+        photos: PropTypes.arrayOf(PropTypes.string),
+      }),
       question_id: PropTypes.number,
       question_body: PropTypes.string,
       question_date: PropTypes.string,
       asker_name: PropTypes.string,
       question_helpfulness: PropTypes.number,
       reported: PropTypes.bool,
-    })),
-  }).isRequired,
-  answers: PropTypes.shape({
-    question: PropTypes.string,
-    results: PropTypes.arrayOf(PropTypes.shape({
-      answer_id: PropTypes.number,
-      body: PropTypes.string,
-      date: PropTypes.string,
-      answerer_name: PropTypes.string,
-      helpfulness: PropTypes.number,
-      photos: PropTypes.arrayOf(PropTypes.string),
-    })),
-  }).isRequired,
+    }),
+  )]),
+  productName: PropTypes.string.isRequired,
+};
+
+QuestionsAnswersList.defaultProps = {
+  questions: PropTypes.object,
 };
 
 export default QuestionsAnswersList;
