0a6e4c9b60e852c4bb9fcfe358aa27bfcf522705
Calvin Chau <cachau33@gmail.com>
2021-08-02 12:43:17 -0700

    Main index no longer passes example data to App

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import productData from '../../exampleProductData';
+// import productData from '../../exampleProductData';
 
 ReactDOM.render(<App />, document.getElementById('root'));
