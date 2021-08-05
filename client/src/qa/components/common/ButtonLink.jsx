edc6d6ea4051c0095d45eacdab2a02b7730bb772
Julie Barwick <julie@juliebarwick.com>
2021-08-05 09:10:17 -0700

    PropTypes fix, styling and add a styled component props for smaller button links

--- client/src/qa/components/common/ButtonLink.jsx
+++ client/src/qa/components/common/ButtonLink.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const ButtonLink = ({ type, label, onClick }) => (
+const ButtonLink = ({
+  type, label, onClick, small,
+}) => (
   <StyledButtonLink
     type={type}
     onClick={onClick}
+    small={small}
   >
     {label}
   </StyledButtonLink>
 );
 
 const StyledButtonLink = styled.button`
+font-size: ${(props) => (props.small ? '0.8em' : 'inherit')};
 background-color: transparent;
 border: none;
 cursor: pointer;
@@ -29,6 +33,11 @@ ButtonLink.propTypes = {
   type: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  small: PropTypes.bool,
+};
+
+ButtonLink.defaultProps = {
+  small: false,
 };
 
 export default ButtonLink;
