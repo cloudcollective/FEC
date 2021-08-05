bf8dc0ccb5c136ae678099d0a1d6740e90abef68
Julie Barwick <julie@juliebarwick.com>
2021-08-05 09:50:12 -0700

    Add some flex styling to help with layout

--- client/src/qa/components/AnswerEntry.jsx
+++ client/src/qa/components/AnswerEntry.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import AnswererInfo from './AnswererInfo';
-import Modal from './common/Modal';
-import useToggle from './common/useToggle';
 import PhotoModal from './PhotoModal';
 
 const AnswerEntry = ({ answer, first }) => (
@@ -26,7 +24,7 @@ const AnswerEntry = ({ answer, first }) => (
       </SecondCol>
     )}
     <SecondCol>
-      <Row>
+      <UserRow>
         <AnswererInfo
           className="user-info"
           answerId={answer.id}
@@ -34,7 +32,7 @@ const AnswerEntry = ({ answer, first }) => (
           date={answer.date}
           helpfulness={answer.helpfulness}
         />
-      </Row>
+      </UserRow>
     </SecondCol>
   </>
 );
@@ -47,15 +45,7 @@ const SecondCol = styled.div`
   grid-column: 2 / span 1;
 `;
 
-const Thumbnail = styled.img`
-  max-width: 180px;
-  max-height: 100px;
-  cursor: pointer;
-  margin: 5px;
-  border: 2px solid black;
-`;
-
-const Row = styled.div`
+const UserRow = styled.div`
   display: flex;
   align-items: baseline;
 `;
