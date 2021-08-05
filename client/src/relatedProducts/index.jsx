eec35cabaf1f9b0861e971f53203db2303e5a60b
Calvin Chau <cachau33@gmail.com>
2021-08-05 09:09:00 -0700

    Implemented related card functionality. User can scroll through product cards.

--- client/src/relatedProducts/index.jsx
+++ client/src/relatedProducts/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 // import PropTypes from 'prop-types';
 // 'prop-types' should be listed in the project's dependencies. Run 'npm i -S prop-types' to add it
 import CardMaker from './components/CardMaker';
 import AddOutfit from './components/AddOutfit';
-// refactor using hooks later
 
 const RPContainer = styled.div`
 border: solid 1px black;
@@ -13,28 +12,89 @@ const CardList = styled.div`
 border: solid 1px black;
 padding: 10px;
 margin: 0 0 0 5px;
+display:flex;
 `;
+
 const RelatedProductsContainer = ({ product, products }) => {
-  let arrayOfCards = [];
-  // let renderElement = () => (
-  //   5
-  // );
-  if (products.length <= 5 && products.length !== 0) {
-    arrayOfCards = products.map((prod) => {
-      const product = {
-        product: prod,
-      };
-      return (
-        CardMaker(product)
-      );
-    });
-  }
+  let arrayOfProductCards = [];
+  const [relatedProdcutData, setRelatedProdcutData] = useState([]);
+  const [productCards, setproductCards] = useState([]);
+  const [displayCards, setDisplayCards] = useState([]);
+  // const [navCardList, setNavCardList] = useState([0, 5]);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(5);
+  const [leftButtonVisibility, setLeftButtonVisibility] = useState(false);
+  const [rightButtonVisibility, setRightButtonVisibility] = useState(false);
+
+  useEffect(() => {
+    setRelatedProdcutData(products);
+  }, [products]);
+
+  useEffect(() => {
+    setproductCards(arrayOfProductCards);
+  }, [relatedProdcutData]);
+
+  useEffect(() => {
+    if (productCards.length === 0) {
+      // decide whether you want to display a dummy card
+    } else if (productCards.length <= 5) {
+      setDisplayCards(productCards);
+    } else {
+      setDisplayCards(productCards.slice(start, end));
+    }
+  }, [productCards, start, end]);
+
+  useEffect(() => {
+    if (productCards.length > 5 && start !== 0) {
+      setLeftButtonVisibility(true);
+    } else {
+      setLeftButtonVisibility(false);
+    }
+  }, [productCards, start]);
+
+  useEffect(() => {
+    if (productCards.length > 5 && end !== productCards.length) {
+      setRightButtonVisibility(true);
+    } else {
+      setRightButtonVisibility(false);
+    }
+  }, [productCards, end]);
+
+  arrayOfProductCards = relatedProdcutData.map((prod) => (
+    <CardMaker
+      product={prod}
+      key={prod.id}
+    />
+  ));
   return (
     <RPContainer>
       <div className="list-row-1">
         <h4><em>Related Products</em></h4>
         <CardList>
-          { arrayOfCards }
+          {leftButtonVisibility
+          && (
+          <button
+            type="button"
+            onClick={() => {
+              setStart(((prevState) => prevState - 1));
+              setEnd(((prevState) => prevState - 1));
+            }}
+          >
+            &lt;
+          </button>
+          )}
+          {displayCards}
+          {rightButtonVisibility && (
+          <button
+            type="button"
+            onClick={() => {
+              setStart(((prevState) => prevState + 1));
+              setEnd(((prevState) => prevState + 1));
+            }}
+          >
+            &gt;
+          </button>
+          )}
         </CardList>
       </div>
       <div className="list-row-2">
@@ -46,15 +106,8 @@ const RelatedProductsContainer = ({ product, products }) => {
     </RPContainer>
   );
 };
-
-// RelatedProductsContainer.propTypes = {
-//   products: PropTypes.object.isRequired,
-// };
-
 export default RelatedProductsContainer;
 /*
-          <CardMaker product={arrayOfObj[0]} />
-          <CardMaker product={arrayOfObj[1]} />
-          <CardMaker product={arrayOfObj[2]} />
-          <CardMaker product={arrayOfObj[3]} />
-*/
+ note to self
+ check for duplicate products!
+  */
