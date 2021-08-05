ed449652aa6b6da6a9f0704401b0ff33accec59d
Calvin Chau <cachau33@gmail.com>
2021-08-05 14:31:23 -0700

    No changes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -116,7 +116,6 @@ class App extends React.Component {
             product={selectedProductData}
             products={relatedProductData}
           />
-<<<<<<< HEAD
         </div>
         <div>
           <QuestionsAnswers
@@ -124,14 +123,6 @@ class App extends React.Component {
             productId={questionsAndAnswersData.product_id}
           />
         </div>
-=======
-        </div> */}
-        { /* TODO Fix Product ID implementation */}
-        <QuestionsAnswers
-          questions={questionsAndAnswersData.results}
-          productId={questionsAndAnswersData.product_id}
-        />
->>>>>>> main
         {/* <div>
          <ReviewsRatings />
          </div> */}
