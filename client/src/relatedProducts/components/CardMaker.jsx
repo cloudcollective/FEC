2e45f86b6e9f3dd9d4b32f50e1ef038961fac254
Calvin Chau <cachau33@gmail.com>
2021-08-06 02:18:55 -0700

    Component now passes name, and features of current and realted product to the button

--- client/src/relatedProducts/components/CardMaker.jsx
+++ client/src/relatedProducts/components/CardMaker.jsx
@@ -10,24 +10,42 @@ width: 250px;
 display: inline-block;
 margin-right: 15px;
 `;
-const CardMaker = ({ product, buttonType, features }) => {
+const CardMaker = ({ product, currentProduct, buttonType }) => {
   const placeholderImage = 'https://westsiderc.org/wp-content/uploads/2019/08/Image-Not-Available.png';
+  const [productData, setProductData] = useState([]);
+  const [currentProductFeatures, setCurrentProductFeatures] = useState([]);
+  const [relatedProductedFeatures, setRelatedProductedFeatures] = useState([]);
+
+  useEffect(() => {
+    const {
+      url, category, name, price,
+    } = product;
+    setProductData([url, category, name, price]);
+  }, [product]);
+  useEffect(() => {
+    const { name, features } = currentProduct;
+    setCurrentProductFeatures([name, features]);
+  }, [currentProduct]);
+  useEffect(() => {
+    const { name, features} = product;
+    setRelatedProductedFeatures([name, features]);
+  }, [product]);
 
   return (
     <Card>
-      <div>
-        {buttonType === 'modal' && (<FeaturesButton />)}
-        {buttonType === 'delete' && (<DeleteButton />)}
-        <img src={`${product.url}`} onError={(e) => { e.target.onerror = null; e.target.src = placeholderImage; }} alt="default" width="200px" height="280px" />
+      {buttonType === 'modal' && (<FeaturesButton currentFeatures={currentProductFeatures} relatedFeatures={relatedProductedFeatures} />)}
+      {buttonType === 'delete' && (<DeleteButton />)}
+      <div className="center-image">
+        <img src={`${productData[0]}`} onError={(e) => { e.target.onerror = null; e.target.src = placeholderImage; }} alt="default" width="210" height="288px" />
       </div>
       <div className="category">
-        {`Category: ${product.category}`}
+        {`Category: ${productData[1]}`}
       </div>
       <div className="product-name">
-        {`Name: ${product.name}`}
+        {`Name: ${productData[2]}`}
       </div>
       <div className="price">
-        {`Price: $${product.price}`}
+        {`Price: $${productData[3]}`}
       </div>
       <div className="stars">
         ★★★★★
