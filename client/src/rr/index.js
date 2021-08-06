b88a7f7f9e45cebbf498be22f3117a019f7c55ad
Calvin Chau <cachau33@gmail.com>
2021-08-06 16:25:54 -0700

    Removed axios, no longer necessary

--- client/src/rr/index.js
+++ client/src/rr/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 import ReviewList from './components/ReviewList';
 import Overview from './components/Overview';
 
