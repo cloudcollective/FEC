5a589757ffe0c7523cf5afe0ce9031139571607c
Calvin Chau <cachau33@gmail.com>
2021-08-06 16:24:36 -0700

    Refactored your index to use hooks, functionality is 100% the same as before. tested the code and nothing is broken, and i kept the names that you used

--- client/src/rr/index.js
+++ client/src/rr/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import ReviewList from './components/ReviewList';
@@ -22,69 +22,43 @@ font-size: 2.5em;
 margin-bottom: 30px;
 `;
 
-class Reviews extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-      results: null,
-      meta: null,
-      average: null,
-      numberOfRatings: null,
-
-    };
-  }
-
-  componentDidMount() {
-    axios.get('/reviews/id', {
-      params: {
-        id: 25171,
-      },
-    })
-      .then(((res) => this.setState({ results: res.data.results }, () => { console.log('reviews recieved and set on state', this.state.results ); })))
-      .catch((err) => console.log(err, 'what reviews'));
-
-    axios.get('/reviews/meta', {
-      params: {
-        id: 25171,
-      },
-    })
-      .then((res) => (this.setState({ meta: res.data }, () => { console.log('metadata received and set on state', this.state.meta);
-        this.setState({ average: this.getAverageRating() },
-          () => {console.log(this.state.average)}); })))
-      .catch((err) => (console.log('what metadata', err)));
-  }
-
-  getAverageRating() {
+const Reviews = ({ reviews, rating }) => {
+  const getAverageRating = (ratings) => {
     let total = 0;
     let numberOfRatings = 0;
-    for (var i = 1; i <= 5; i++) {
-      if(this.state.meta.ratings[i]) {
-        total += (parseInt(this.state.meta.ratings[i]) * i);
-        numberOfRatings += parseInt(this.state.meta.ratings[i]);
+    for (let i = 1; i <= 5; i += 1) {
+      if (ratings[i]) {
+        total += (parseInt(ratings[i]) * i);
+        numberOfRatings += parseInt(ratings[i]);
       }
     }
-    this.setState({ numberOfRatings: numberOfRatings }, () => {console.log('number of ratings set to :' + this.state.numberOfRatings)});
-    let average = total / numberOfRatings;
-    return average;
-  }
+    setNumberOfratings(numberOfRatings);
+    const average = total / numberOfRatings;
+    setAverage(average);
+  };
 
-  render() {
-    if (this.state.results === null || this.state.meta === null) {
-      return (
-      <>loading</>
-      );
-    }
-    return (
+  const [reviewsArray, setReviewsArray] = useState([]);
+  const [ratings, setRatings] = useState({});
+  const [numberOfRatings, setNumberOfratings] = useState(0);
+  const [average, setAverage] = useState(0);
+
+  useEffect(() => {
+    setReviewsArray(reviews);
+  }, [reviews]);
+
+  useEffect(() => {
+    setRatings(rating);
+    getAverageRating(rating);
+  }, [rating]);
+
+  return (
     <FullComponent>
       <h3 className="widget-title">Ratings &#38; Reviews</h3>
       <Container>
-        <Overview ratings={this.state.meta.ratings} average={this.state.average} numberOfRatings={this.state.numberOfRatings}/>
-        <ReviewList results={this.state.results} />
+        <Overview ratings={ratings} average={average} numberOfRatings={numberOfRatings} />
+        <ReviewList results={reviewsArray} />
       </Container>
     </FullComponent>
-    );
-  }
-}
-
+  );
+};
 export default Reviews;
