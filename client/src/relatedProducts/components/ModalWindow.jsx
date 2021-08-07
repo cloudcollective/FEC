f428747cd44eb3e0da3f8322714ca9659e43e653
Calvin Chau <cachau33@gmail.com>
2021-08-07 01:27:06 -0700

    This is almost done.

--- client/src/relatedProducts/components/ModalWindow.jsx
+++ client/src/relatedProducts/components/ModalWindow.jsx
@@ -1,78 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
-import noDuplicates from './noDuplicates';
+import ModalFeatures from './ModalFeatures';
 import Modal from '../../qa/components/common/Modal';
+
+// const featuresContainer = styled.div`
+//   display:flex;
+//   `;
+// const featuresColumn = styled.div`
+//   height:100px;
+//   width: 33px;
+//   border: solid 1px black;
+//   `;
 
 const featuresModal = ({
   currentFeatures, relatedFeatures, isVisible, setIsVisible,
 }) => {
-  const featuresContainer = styled.div`
-  display:flex;
-  `;
-  const featuresColumn = styled.div`
-  height:100px;
-  width: 33px;
-  border: solid 1px black;
-  `;
-
-  const makeEqual = (arrSmall, arrBig) => {
-    while (arrBig.length > arrSmall.length) {
-      arrSmall.push('empty');
-    }
-    return arrSmall;
-  };
-  const check = <div>✓</div>;
-  const empty = <div> </div>;
-
-  const leftColumn = [];
-  const middleColumn = [];
-  const rightColumn = [];
-
-  leftColumn.push(<div>{currentFeatures[0]}</div>);
-  middleColumn.push(<div>Features</div>);
-  rightColumn.push(<div>{relatedFeatures[0]}</div>);
-  if (Array.isArray(currentFeatures[1])) {
-    const allFeatures = noDuplicates(currentFeatures[1].concat(relatedFeatures[1]));
-    let lTemp = currentFeatures[1].slice();
-    let rTemp = relatedFeatures[1].slice();
-    lTemp = makeEqual(lTemp, allFeatures);
-    rTemp = makeEqual(rTemp, allFeatures);
-    for (let i = 0; i < allFeatures.length; i += 1) {
-      if (typeof lTemp[i] === 'object') {
-        const { feature, value } = lTemp[i];
-        const characteristic = `${feature} ${value}`;
-        if (characteristic === allFeatures[i]) {
-          leftColumn.push(check);
-        } else {
-          leftColumn.push(empty);
-        }
-      } else {
-        leftColumn.push(empty);
-      }
-      if (typeof rTemp[i] === 'object') {
-        const { feature, value } = rTemp[i];
-        const characteristic = `${feature} ${value}`;
-        if (characteristic === allFeatures[i]) {
-          leftColumn.push(check);
-        } else {
-          leftColumn.push(empty);
-        }
-      } else {
-        leftColumn.push(empty);
-      }
-    }
+  let features = [];
+  if (currentFeatures.length) {
+    features = ModalFeatures(currentFeatures, relatedFeatures);
   }
-
   return (
     <Modal
       isVisible={isVisible}
       setIsVisible={setIsVisible}
     >
-      <featuresContainer>
-        <featuresColumn>{leftColumn}</featuresColumn>
-        <featuresColumn>{middleColumn}</featuresColumn>
-        <featuresColumn>{rightColumn}</featuresColumn>
-      </featuresContainer>
+      <table>
+        <thread>
+          <tr>
+            <th colSpan="3">Features Overview</th>
+          </tr>
+        </thread>
+        <tbody>
+          <tr>
+            <td className="left">{currentFeatures[0]}</td>
+            <td>Features</td>
+            <td className="right">{relatedFeatures[0]}</td>
+          </tr>
+          {/* <tr>
+            <td>V</td>
+            <td>with two columns</td>
+            <td>X</td>
+
+          </tr> */}
+          {features}
+        </tbody>
+      </table>
     </Modal>
   );
 };
