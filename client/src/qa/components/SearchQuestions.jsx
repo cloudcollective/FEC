1818708dbfda854cfdf41c63b163185190e400b8
Julie Barwick <julie@juliebarwick.com>
2021-08-07 11:39:47 -0700

    Implement search icon and onfocus/onblur change for search bar

--- client/src/qa/components/SearchQuestions.jsx
+++ client/src/qa/components/SearchQuestions.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { FaSearch } from 'react-icons/fa';
 
 const SearchQuestions = ({ handleSearchQuestions, doReset }) => {
   const [filter, setFilter] = useState('');
+  const [focus, setFocus] = useState(false);
 
   const changeAndFilter = (e) => {
     const text = e.target.value;
@@ -16,29 +18,63 @@ const SearchQuestions = ({ handleSearchQuestions, doReset }) => {
     }
   };
 
+  const handleOnFocus = () => {
+    setFocus(true);
+  };
+
+  const handleOnBlur = () => {
+    setFocus(false);
+  };
+
   return (
-    <StyledInput
-      className="search-question"
-      type="search"
-      placeholder="Have a question? Search for Answers..."
-      value={filter}
-      onChange={changeAndFilter}
-    />
+    <SearchBox
+      focus={focus}
+      onFocus={handleOnFocus}
+      onBlur={handleOnBlur}
+    >
+      <StyledInput
+        className="search-question"
+        type="search"
+        placeholder="Have a question? Search for Answers..."
+        value={filter}
+        onChange={changeAndFilter}
+      />
+      <SearchIconBox>
+        <FaSearch />
+      </SearchIconBox>
+    </SearchBox>
   );
 };
 
-const StyledInput = styled.input`
-  display: block;
+const SearchBox = styled.div`
+  display: flex;
+  justify: flex-start;
+  align-items: center;
+  border: ${(props) => (props.focus ? '1px solid #272727' : '1px solid #767676')};
   width: 100%;
+  ${'' /* padding: .375rem .75rem; */}
+  font-size: 1rem;
+  line-height: 2;
+  background-color: #fff;
+  padding: 0;
+`;
+
+const SearchIconBox = styled.div`
+  flex-grow: 1;
+  text-align: center;
+  padding-top: 1px;
+`;
+
+const StyledInput = styled.input`
+  flex-grow: 15;
   padding: .375rem .75rem;
   font-size: 1rem;
   line-height: 2;
   color: #272727;
   background-color: #fff;
-  border: 1px solid #767676;
+  border: none;
   &:focus {
     outline: none;
-    border: 1px solid #272727;
   }
 `;
 
